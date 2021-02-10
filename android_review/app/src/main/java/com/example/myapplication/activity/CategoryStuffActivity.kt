22301237ba9ce36af63e36eb77ea0b798a493b25
jojo <z867793137@icloud.com>
2021-02-10 17:58:27 +0800

    个人页面详细信息

--- android_review/app/src/main/java/com/example/myapplication/activity/CategoryStuffActivity.kt
+++ android_review/app/src/main/java/com/example/myapplication/activity/CategoryStuffActivity.kt
@@ -1,12 +1,36 @@
 package com.example.myapplication.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import cn.bmob.v3.BmobQuery
+import cn.bmob.v3.exception.BmobException
+import cn.bmob.v3.listener.FindListener
 import com.example.myapplication.R
+import com.example.myapplication.adapter.StuffAdapter
+import com.example.myapplication.bean.Stuff
+import kotlinx.android.synthetic.main.activity_category_stuff.*
+import java.util.*
 
 class CategoryStuffActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_stuff)
+        category_stuff.layoutManager = LinearLayoutManager(this)
+        category_stuff.adapter =
+            StuffAdapter(getStuffList(intent.getStringExtra("categoryId")), this)
     }
+
+    private fun getStuffList(categoryId: String?): List<Stuff>? {
+        val bmobQuery = BmobQuery<Stuff>()
+        bmobQuery.addWhereEqualTo("categoryId", categoryId)
+        val result: MutableList<Stuff> = ArrayList()
+        bmobQuery.findObjects(object : FindListener<Stuff>() {
+            override fun done(list: List<Stuff>, e: BmobException) {
+                result.addAll(list)
+            }
+        })
+        return result
+    }
+
 }
--- android_review/app/src/main/java/com/example/myapplication/bottomnavigation/my/MyFragment.java
+++ android_review/app/src/main/java/com/example/myapplication/bottomnavigation/my/MyFragment.java
@@ -47,20 +47,6 @@ public class MyFragment extends Fragment implements View.OnClickListener {
         myUserNameTextView = root.findViewById(R.id.tv_myUserName);
         myUserInfoLinearLayout = root.findViewById(R.id.ly_myUserInfo);
 
-        userinfo = getContext().getSharedPreferences(SharePreferencesUtils.USER_INFORMATION_FILE, MODE_PRIVATE);
-        String localUserId = userinfo.getString("userId", null);
-        String localUserLoginId = userinfo.getString("userLoginId", null);
-        String localUserName = userinfo.getString("userName", null);
-        String localUserPhoneNum = userinfo.getString("userPhoneNum", null);
-        String localUserEmail = userinfo.getString("userEmail", null);
-        String localUserRegisterDate = userinfo.getString("userRegisterDate", null);
-
-        if (localUserName != null) {
-            myUserNameTextView.setText(localUserName);
-        }
-
-        // 初始化用于美化UI的点击事件
-        initUIClick(root);
 
         // 查看我收藏的事件绑定
         myCollectionImageView.setOnClickListener(this);
@@ -74,8 +60,6 @@ public class MyFragment extends Fragment implements View.OnClickListener {
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharePreferencesUtils.clear(getContext(), SharePreferencesUtils.USER_INFORMATION_FILE);
-
                 Intent intent = new Intent(getContext(), LoginActivity.class);
                 startActivity(intent);
                 Toast.makeText(getContext(), "退出登录", Toast.LENGTH_SHORT).show();
@@ -85,74 +69,12 @@ public class MyFragment extends Fragment implements View.OnClickListener {
         return root;
     }
 
-    private void initUIClick(View view) {
-        view.findViewById(R.id.iv_myOtherFunction01).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction01).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction02).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction02).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction03).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction03).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction04).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction04).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction05).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction05).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction06).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction06).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction07).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction07).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction08).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction08).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction09).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction09).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction10).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction10).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction11).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction11).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction12).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction12).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction13).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction13).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction14).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction14).setOnClickListener(this);
-        view.findViewById(R.id.iv_myOtherFunction15).setOnClickListener(this);
-        view.findViewById(R.id.tv_myOtherFunction15).setOnClickListener(this);
-    }
-
     @Override
     public void onClick(View v) {
         Intent intent = null;
         switch (v.getId()) {
-            case R.id.iv_myOtherFunction01:
-            case R.id.tv_myOtherFunction01:
-            case R.id.iv_myOtherFunction02:
-            case R.id.tv_myOtherFunction02:
-            case R.id.iv_myOtherFunction03:
-            case R.id.tv_myOtherFunction03:
-            case R.id.iv_myOtherFunction04:
-            case R.id.tv_myOtherFunction04:
-            case R.id.iv_myOtherFunction05:
-            case R.id.tv_myOtherFunction05:
-            case R.id.iv_myOtherFunction06:
-            case R.id.tv_myOtherFunction06:
-            case R.id.iv_myOtherFunction07:
-            case R.id.tv_myOtherFunction07:
-            case R.id.iv_myOtherFunction08:
-            case R.id.tv_myOtherFunction08:
-            case R.id.iv_myOtherFunction10:
-            case R.id.tv_myOtherFunction10:
-            case R.id.iv_myOtherFunction11:
-            case R.id.tv_myOtherFunction11:
-            case R.id.iv_myOtherFunction12:
-            case R.id.tv_myOtherFunction12:
-            case R.id.iv_myOtherFunction13:
-            case R.id.tv_myOtherFunction13:
-            case R.id.iv_myOtherFunction14:
-            case R.id.tv_myOtherFunction14:
-            case R.id.iv_myOtherFunction15:
-            case R.id.tv_myOtherFunction15:
-                Toast.makeText(getContext(), "You clicked me!", Toast.LENGTH_SHORT).show();
-                break;
             case R.id.btn_showMyInfoDetail:
+                break;
             case R.id.ly_myUserInfo:
                 // 简单个人信息展示点击进入详细信息展示页
                 intent = new Intent(getContext(), MyDetailInfoActivity.class);
@@ -163,18 +85,6 @@ public class MyFragment extends Fragment implements View.OnClickListener {
                 intent = new Intent(getContext(), MyCollectedListActivity.class);
                 startActivity(intent);
                 break;
-        }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart");
-        // 更新完用户信息并退出详情页时，该活动的该Fragment处于栈顶显示，此时更新用户名保持信息一致性
-        String localUserName = userinfo.getString("userName", null);
-
-        if (localUserName != null) {
-            myUserNameTextView.setText(localUserName);
         }
     }
 }
