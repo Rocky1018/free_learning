17a0dd71b820a47b2702af8198d7ca9fa6ef161d
jojo <z867793137@icloud.com>
2021-02-10 14:39:32 +0800

    管理页面微调

--- android_review/app/src/main/java/com/example/myapplication/activity/AdminActivity.kt
+++ android_review/app/src/main/java/com/example/myapplication/activity/AdminActivity.kt
@@ -1,9 +1,15 @@
 package com.example.myapplication.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.myapplication.R
+import com.example.myapplication.utils.SharePreferencesUtils
 
 class AdminActivity : AppCompatActivity() {
 
@@ -22,6 +28,17 @@ class AdminActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            findPreference<Preference>("user_info")?.onPreferenceClickListener =
+                Preference.OnPreferenceClickListener {
+                    startActivity(Intent(Settings.ACTION_DISPLAY_SETTINGS))
+                    false
+                }
+            findPreference<EditTextPreference>("password")?.onPreferenceChangeListener =
+                Preference.OnPreferenceChangeListener { preference, newValue ->
+                    Log.d("terminal_number", "preference:$preference,newValue:$newValue")
+                    //SharePreferencesUtils.setParam(activity, "terminal_number", newValue.toString())
+                    true
+                }
         }
     }
 }
